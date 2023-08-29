6bf1125585f5c8fcf25326b9c260d63ce240f3ad
giho <tom1130@naver.com>
2023-08-29 16:54:55 +0900

    monitoring pipeline

--- examples/source/monitoring/monitoring.py
+++ examples/source/monitoring/monitoring.py
@@ -4,12 +4,10 @@ import json
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-import boto3
 import pandas as pd
-import numpy as np
 from sklearn.metrics import r2_score
 
-class monitoring:
+class Monitoring:
     
     def __init__(self, args):
         self.args = args
@@ -21,13 +19,13 @@ class monitoring:
 
         # fr
         fr = self.df[self.df['lounge']=='FR']
-        fr_r2 = r2_score(fr['predictions'], fr['lounge'])
+        fr_r2 = r2_score(fr['predictions'], fr['label'])
         # mr
         mr = self.df[self.df['lounge']=='MR']
-        mr_r2 = r2_score(mr['predictions'], mr['lounge'])
+        mr_r2 = r2_score(mr['predictions'], mr['label'])
         # pr
         pr = self.df[self.df['lounge']=='PR']
-        pr_r2 = r2_score(pr['predictions'], pr['lounge'])
+        pr_r2 = r2_score(pr['predictions'], pr['label'])
 
         report_dict = {
             "r2" :
@@ -38,13 +36,15 @@ class monitoring:
                 }
         }
 
-        # fr pipeline start
+        # fr training pipeline execute if needed
         if fr_r2 < self.args.floatFrThreshold:
             pass
-
+        
+        # mr training pipeline execute if needed
         if mr_r2 < self.args.floatMrThreshold:
             pass
-
+        
+        # pr training pipeline execute if needed 
         if pr_r2 < self.args.floatPrThreshold:
             pass
 
@@ -54,25 +54,27 @@ class monitoring:
             f.write(json.dumps(report_dict))
 
     def execution(self):
-        self._read_predictions()
         self._read_label()
+        self._read_predictions()
+
+        self.logic()
 
     def _read_predictions(self):
-        list_dir = os.listdir(os.path.join(self.args.strDataPath,'input'))
+        list_dir = os.listdir(os.path.join(self.args.strDataPath,'input','predictions'))
         min_date = self.min_date.replace('-','')
         max_date = self.max_date.replace('-','')
         df = pd.DataFrame(columns=['std','group','predictions','lounge'])
 
         for dir in list_dir:
             if dir>=min_date and dir<max_date:
-                prediction = pd.read_csv(os.path.join(self.args.strDataPath,'input',dir,self.args.strDataName))
+                prediction = pd.read_csv(os.path.join(self.args.strDataPath,'input','predictions',dir,self.args.strDataName))
                 prediction = prediction[prediction['std']==prediction['std'].min()]
                 df = pd.concat([df, prediction])
         self.df = df
 
     def _read_label(self):
         # read csv file
-        self.label = pd.read_csv(self.args.strDataPath, 'input', 'label', 'lounge.csv')
+        self.label = pd.read_csv(os.path.join(self.args.strDataPath, 'input', 'label', self.args.strLabelName))
         
         # get date
         self.max_date = self.label['_date'].max()
@@ -94,7 +96,8 @@ class monitoring:
 
         df_agg.index.name = 'std'
         df_agg = df_agg.rename(columns = {'time_group' : 'group'})
-
+        
+        df_agg = df_agg.reset_index()
         # unpivoting lounge column
         df_agg = pd.melt(df_agg, id_vars=['std','group'], var_name='lounge', value_name='label')
         return df_agg
@@ -104,6 +107,7 @@ if __name__=='__main__':
 
     parser.add_argument('--strDataPath', default='/opt/ml/processing')
     parser.add_argument('--strDataName', default='pnr.csv')
+    parser.add_argument('--strLabelName', default='lounge.csv')
     parser.add_argument('--strOutputName', default='monitoring.json')
     # fr
     parser.add_argument('--floatFrThreshold', default=0.7)
@@ -111,3 +115,7 @@ if __name__=='__main__':
     parser.add_argument('--floatMrThreshold', default=0.95)
     # pr
     parser.add_argument('--floatPrThreshold', default=0.95)
+
+    args = parser.parse_args()
+    monitoring = Monitoring(args)
+    monitoring.execution()
