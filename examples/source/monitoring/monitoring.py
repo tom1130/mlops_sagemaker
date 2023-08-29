62d5241122865b12b4abac743cb25fdd106ae4dc
giho <tom1130@naver.com>
2023-08-29 09:28:01 +0900

    monitoring

--- examples/source/monitoring/monitoring.py
+++ examples/source/monitoring/monitoring.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import json
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -15,10 +16,42 @@ class monitoring:
         
         
     def logic(self):
-        # label column = ['std','group','FR_LNG",'MR_LNG,PR_LNG]
+        # label column = ['std','group','lounge', 'label]
         self.df = self.df.merge(self.label, on=['std','group','lounge'], how='inner')
-        
-        pass
+
+        # fr
+        fr = self.df[self.df['lounge']=='FR']
+        fr_r2 = r2_score(fr['predictions'], fr['lounge'])
+        # mr
+        mr = self.df[self.df['lounge']=='MR']
+        mr_r2 = r2_score(mr['predictions'], mr['lounge'])
+        # pr
+        pr = self.df[self.df['lounge']=='PR']
+        pr_r2 = r2_score(pr['predictions'], pr['lounge'])
+
+        report_dict = {
+            "r2" :
+                {
+                    'FR' : fr_r2,
+                    'MR' : mr_r2,
+                    'PR' : pr_r2
+                }
+        }
+
+        # fr pipeline start
+        if fr_r2 < self.args.floatFrThreshold:
+            pass
+
+        if mr_r2 < self.args.floatMrThreshold:
+            pass
+
+        if pr_r2 < self.args.floatPrThreshold:
+            pass
+
+        # save monitoring data
+        output_path = os.path.join(self.args.strDataPath, 'output', self.args.strOutputName)
+        with open(output_path, 'w') as f:
+            f.write(json.dumps(report_dict))
 
     def execution(self):
         self._read_predictions()
@@ -62,15 +95,19 @@ class monitoring:
         df_agg.index.name = 'std'
         df_agg = df_agg.rename(columns = {'time_group' : 'group'})
 
-        # melt 함수 사용
-
+        # unpivoting lounge column
+        df_agg = pd.melt(df_agg, id_vars=['std','group'], var_name='lounge', value_name='label')
         return df_agg
     
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='monitoring')
+
+    parser.add_argument('--strDataPath', default='/opt/ml/processing')
+    parser.add_argument('--strDataName', default='pnr.csv')
+    parser.add_argument('--strOutputName', default='monitoring.json')
     # fr
-    parser.add_argument('--strFrLoungePath', default='')
+    parser.add_argument('--floatFrThreshold', default=0.7)
     # mr
-    parser.add_argument('--strMrLoungePath', default='')
+    parser.add_argument('--floatMrThreshold', default=0.95)
     # pr
-    parser.add_argument('--strPrLoungePath', default='')
+    parser.add_argument('--floatPrThreshold', default=0.95)
